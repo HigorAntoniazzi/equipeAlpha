09ba5c8ddd6ef45a85d601c277f4f213231d01b4
HigorAntoniazzi <133401020+HigorAntoniazzi@users.noreply.github.com>
2023-10-24 20:51:32 -0300

    Atualização de sistema 2.0

--- App.js
+++ App.js
@@ -11,12 +11,12 @@ export default function App() {
         <Text style={styles.title}>Listas 2</Text>
       </View>
 
-      <Box corView='#ffa4a4' corTexto={'red'} mensagemAnimais={'Animais'}></Box>
-      <Box corView='#eda4ff' corTexto={'purple'} mensagemAnimais={'Carros'}></Box>
-      <Box corView='#a4d4ff' corTexto={'blue'} mensagemAnimais={'Peixes'}></Box>
-      <Box corView='#a4ffc3' corTexto={'green'} mensagemAnimais={'Times'}></Box>
-      <Box corView='#d1d6d3' corTexto={'gray'} mensagemAnimais={'Filmes'}></Box>
-      <Box corView='#e8efbd' corTexto={'#747d10'} mensagemAnimais={'Linguagem'}></Box>
+      <Box corView='#ffa4a4' corTexto='red' mensagemAnimais='Animais'></Box>
+      <Box corView='#eda4ff' corTexto='purple' mensagemAnimais='Carros'></Box>
+      <Box corView='#a4d4ff' corTexto='blue' mensagemAnimais='Peixes'></Box>
+      <Box corView='#a4ffc3' corTexto='green' mensagemAnimais='Times'></Box>
+      <Box corView='#d1d6d3' corTexto='gray' mensagemAnimais='Filmes'></Box>
+      <Box corView='#e8efbd' corTexto='#747d10' mensagemAnimais='Linguagem'></Box>
     </View>
   );
 }
